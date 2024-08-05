0792da428073355d35ecdbd0ec5e189da5cddb26
Fuma1322 <leonardfuma26@gmail.com>
2024-08-05 16:05:41 +0200

    boiler plate

---
+++ app/(front)/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Hero from '@/components/FrontEnd/Hero'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
