b5bfe6aa8a72bf76bc9d885cf38042ceaa5449ca
Fuma1322 <leonardfuma26@gmail.com>
2024-08-06 13:36:15 +0200

    overview added as link

---
+++ app/(front)/overview/page.tsx
@@ -0,0 +1,10 @@
+import Overview from '@/components/FrontEnd/ProductsOverview'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Overview />
+    </div>
+  )
+}
