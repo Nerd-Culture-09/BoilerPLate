b590fa848dac7ef90df29e68f903c056cb1e3dd1
bibo <masoabibatloung@gmail.com>
2024-08-06 22:59:27 +0200

    add productlists

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,11 +1,13 @@
 'use client'
 import Hero from '@/components/FrontEnd/Hero'
+import ProductLists from '@/components/FrontEnd/productlists'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
       <Hero />
+      <ProductLists/>
     </div>
   )
 }
