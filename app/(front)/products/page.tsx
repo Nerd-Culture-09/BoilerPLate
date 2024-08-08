2486006ce5461129471f8ff65b780d7c8473d827
Fuma1322 <leonardfuma26@gmail.com>
2024-08-08 09:52:52 +0200

    products page for shops

---
+++ app/(front)/products/page.tsx
@@ -0,0 +1,10 @@
+import { Products } from '@/components/FrontEnd/Products'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <Products />
+    </div>
+  )
+}
--- components/FrontEnd/Shops.tsx
+++ components/FrontEnd/Shops.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from "next/link";
 import { DirectionAwareHover } from "../ui/direction-aware-hover";
 import Footer from "./Footer"
 
@@ -11,17 +12,18 @@ const Shops = () => {
         <div className="px-72">
         <div className="flex items-center justify-center py-4 md:py-8 flex-wrap">
             <button type="button" className="text-blue-700 hover:text-white border border-blue-600 bg-white hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:bg-gray-900 dark:focus:ring-blue-800">All categories</button>
-            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Shoes</button>
-            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Bags</button>
-            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Electronics</button>
-            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Gaming</button>
+            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Clothing</button>
+            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Food</button>
+            <button type="button" className="text-gray-900 border border-white hover:border-gray-200 dark:border-gray-900 dark:bg-gray-900 dark:hover:border-gray-700 bg-white focus:ring-4 focus:outline-none focus:ring-gray-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3 dark:text-white dark:focus:ring-gray-800">Accessories</button>
         </div>
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
             <div className="h-[40rem] relative  flex items-center justify-center">
+                <Link href='/products'>
                 <DirectionAwareHover imageUrl={imageUrl}>
                     <p className="font-bold text-xl">Nucleus Garments</p>
                     <p className="font-normal text-sm">Shop</p>
                 </DirectionAwareHover>
+                </Link>
             </div>
             <div className="h-[40rem] relative  flex items-center justify-center">
                 <DirectionAwareHover imageUrl={imageUrl2}>
