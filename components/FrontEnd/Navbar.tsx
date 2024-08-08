07f8d32fddbabfd7f6adce78a66a2441cdd8b07f
Fuma1322 <leonardfuma26@gmail.com>
2024-08-08 23:32:51 +0200

    ui

--- components/FrontEnd/Navbar.tsx
+++ components/FrontEnd/Navbar.tsx
@@ -1,20 +1,12 @@
 'use client'
 
 import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
   Popover,
   PopoverButton,
   PopoverGroup,
   PopoverPanel,
-  Tab,
-  TabGroup,
-  TabList,
-  TabPanel,
-  TabPanels,
 } from '@headlessui/react'
-import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import { useState } from 'react'
 
 const navigation = {
