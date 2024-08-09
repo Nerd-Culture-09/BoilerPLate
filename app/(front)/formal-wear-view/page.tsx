6faa72a89a0626a943b18b18f5241f469d5e57f4
Stledy <152036434+Stledy@users.noreply.github.com>
2024-08-09 19:09:42 +0200

    formal update

---
+++ app/(front)/formal-wear-view/page.tsx
@@ -0,0 +1,10 @@
+import { FormalProducts } from '@/components/FrontEnd/Formal-wear'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='md-bg-black'>
+        <FormalProducts />
+    </div>
+  )
+}
