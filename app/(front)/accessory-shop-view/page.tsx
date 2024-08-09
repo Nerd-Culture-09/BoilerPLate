e313d798e1156465907373520e6a355b5833af69
Stledy <152036434+Stledy@users.noreply.github.com>
2024-08-09 16:27:09 +0200

    accessorystock update

---
+++ app/(front)/accessory-shop-view/page.tsx
@@ -0,0 +1,10 @@
+import AccessoryShops from '@/components/FrontEnd/accessory-shops'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='md-bg-black'>
+        <AccessoryShops />
+    </div>
+  )
+}
---
+++ app/(front)/cam-items-view/page.tsx
@@ -0,0 +1,11 @@
+
+import { CameraItems } from '@/components/FrontEnd/camera-items'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='md-bg-black'>
+       <CameraItems/>
+    </div>
+  )
+}
